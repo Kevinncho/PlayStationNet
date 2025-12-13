3498a1ff8b2441f48a085ec8fa479061793fb145
SantiBEEP <santiagobadajozdelgado@gmail.com>
2025-12-13 13:14:13 +0100

    Initialize SpringBoot project structure

---
+++ back_playstationet/src/main/java/com/kefessan/playstationet/PlaystationetApplication.java
@@ -0,0 +1,13 @@
+package com.kefessan.playstationet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaystationetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaystationetApplication.class, args);
+	}
+
+}
---
+++ back_playstationet/src/test/java/com/kefessan/playstationet/PlaystationetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kefessan.playstationet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaystationetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
