85bc298d971053fab010942416b94eed9ddd6eca
FabriSnchz <fabri_k-po20@hotmail.com>
2025-12-20 13:47:21 +0100

    Replace placeholder template with main app layout

--- front_playstationet/src/app/app.ts
+++ front_playstationet/src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Navbar } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Home, Login, Sidebar, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('playstationet');
+  showSidebar = false;
+  selectedApp = 'home';
+  isLoggedIn = false;
+
+  onLoginSuccess() {
+    this.isLoggedIn = true;
+  }
+  logout() {
+    this.isLoggedIn = false;
+  }
+  changeApp(app: string) {
+    this.selectedApp = app;
+  }
 }
