dc19d4d9721d7f3c2009922b941595dfff573af3
FabriSnchz <fabri_k-po20@hotmail.com>
2025-12-20 13:47:04 +0100

    Add initial layout and page components

---
+++ front_playstationet/src/app/layout/sidebar/sidebar.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.scss',
+})
+export class Sidebar {
+  @Input() show: boolean = false;
+}
