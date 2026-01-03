57eacba77b03f83144bd80ad7bf0a994512b5e88
FabriSnchz <fabri_k-po20@hotmail.com>
2026-01-03 14:22:47 +0100

    Add GeneralService for API interactions and centralized error handling

--- front_playstationet/src/app/core/config/app.config.ts
+++ front_playstationet/src/app/core/config/app.config.ts
@@ -1,16 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { providePrimeNG } from 'primeng/config';
+import { MessageService } from 'primeng/api';
 import Aura from '@primeuix/themes/aura';
-
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     providePrimeNG({
       theme: {
-        preset: Aura
-      }
-    })
-  ]
+        preset: Aura,
+      },
+    }),
+    MessageService,
+  ],
 };
