3e90e3d9fe59442840af14ab7a1d41b8ab2bea66
Steven <145448953+StevenSimEEP@users.noreply.github.com>
2026-01-03 14:21:55 +0100

    Empezando el login

---
+++ front_playstationet/src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@angular/core";
+import { Observable, of, throwError } from "rxjs";
+import { delay, tap } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  login(email: string, password: string): Observable<any> {
+
+    //Simulación backend
+    if (email === 'admin@gamestore.com' && password === '1234') {
+      return of({
+        token: 'FAKE_JWT_TOKEN_ABC123',
+        user: {
+          email,
+          role: 'ADMIN'
+        }
+      }).pipe(
+        delay(100),
+        tap(response => {
+          localStorage.setItem('token', response.token);
+        })
+      );
+    }
+
+    return throwError(() => new Error('Invalid credentials'));
+  }
+
+  logout(): void{
+    localStorage.removeItem('token');
+  }
+
+  isLogged(): boolean {
+    return !!localStorage.getItem('token');
+  }
+}
--- front_playstationet/src/app/pages/login/login.ts
+++ front_playstationet/src/app/pages/login/login.ts
@@ -1,11 +1,62 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ToastModule } from 'primeng/toast';
+import { AuthService } from '../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    CardModule,
+    ToastModule
+  ],
+  providers: [MessageService],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
+  email: string = '';
+  password: string = '';
+  loading = false;
+
+  constructor(
+    private authService: AuthService,
+    private messageService: MessageService
+  ) {}
+
+  login(form: NgForm) {
+    if (form.invalid) return;
+
+    this.loading = true;
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: () =>{
+        this.loading = false;
+        this.messageService.add({
+          severity: 'succes',
+          summary: 'Login succesful',
+          detail: 'Welcome to the Game Store'
+        });
+      },
+      error: () => {
+        this.loading = false;
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Login failed',
+          detail: 'Invalid email or password'
+        });
+      }
+    })
+  }
 }
